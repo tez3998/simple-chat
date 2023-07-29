d99d31ea0d75cf6f4350df1ac988aaae3477a812
Yuta Tezuka <yuta.tzk@gmail.com>
2023-07-29 16:56:53 +0900

    Allow icon to be opened as image

--- SimpleChat/src/main/java/com/example/simplechat/controllers/UserController.java
+++ SimpleChat/src/main/java/com/example/simplechat/controllers/UserController.java
@@ -7,13 +7,14 @@ import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpEntity;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.example.simplechat.beans.SessionControl;
@@ -92,9 +93,9 @@ public class UserController {
 		return mav;
 	}
 	
-	@GetMapping("/image/{id}")
-	public HttpEntity<byte[]> getIcon(@PathVariable("id") Integer id) {
+	@GetMapping(value = "/image/{id}", produces = MediaType.IMAGE_JPEG_VALUE)
+	public @ResponseBody byte[] getIcon(@PathVariable("id") Integer id) {
 		byte[] icon = this.userRepository.findIconById(id);
-		return new HttpEntity<byte[]>(icon);
+		return icon;
 	}
 }
